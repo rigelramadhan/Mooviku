3463eb4f7a32219a6644a73a944f51dd6bbbfb05
Rigel Ramadhan <rigelramadhan@gmail.com>
2023-03-23 19:15:24 +0700

    Initial commit

---
+++ app/src/main/java/com/rivibi/mooviku/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rivibi.mooviku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
